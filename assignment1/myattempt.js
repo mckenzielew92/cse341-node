1afd3563c71bbe553d601133c3230d9a5dce153c
mckenzielew92 <mckenzielew92@yahoo.com>
2022-01-08 16:13:23 -0700

    adding assignment attempt and solution

---
+++ assignment1/myattempt.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const server = http.createServer();
+server.listen(3000);
+const fs = require('fs');
+
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Greeting</title><head>');
+        res.write('<body><h1>Hello, and Welcome!</h1></body>')
+        res.write('<body><form action="/create-user" method="POST">Username:<input type="text" name="username"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/users' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const username = parsedBody.split('=')[1];
+            fs.writeFile('username.txt', username, (err) => {
+                res.statusCode = 302; //302 stands for redirection
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    
+    }
+    //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!<h1><body>');
+    res.write('</html>');
+    res.end();
+};
