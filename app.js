47a0fb9adb06e29831f8c781c17653f17c2607df
mckenzielew92 <mckenzielew92@yahoo.com>
2022-01-15 11:46:29 -0700

    academind changed week 2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname, 'public'))); //this will point to the public folder as the 'root' folder
 app.use('/admin', adminRoutes); //this /admin will be added to urls in the admin page, but will be ignored in the browser
 
 app.use(shopRoutes);
--- assignment3/myattempt/myattempt.js
+++ assignment3/myattempt/myattempt.js
@@ -2,6 +2,7 @@ express = require('express');
 path = require('path');
 const mainRoutes = require('./routes/index');
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(mainRoutes);
 app.listen(2000);
 
