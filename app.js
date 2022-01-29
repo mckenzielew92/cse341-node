3a1de6c075b69f09687701c5c7a9935b9e63fa48
mckenzielew92 <mckenzielew92@yahoo.com>
2022-01-28 21:46:06 -0700

    admit edit changes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const User = require('./models/user');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -18,20 +18,26 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('61f1e0e0f28a2a569023fcbc')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('61f1e0e0f28a2a569023fcbc')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose
+.connect(
+  'mongodb+srv://utesfan:Utes4l!fe@academind.5ssop.mongodb.net/shop'
+)
+.then(results => {
   app.listen(3000);
+}).catch(err => {
+  console.log(err)
 });
